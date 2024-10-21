ea9c966c606c7580384a4c0142d318dec624a0c0
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-10-21 21:57:28 +0530

    caeser encryption

--- cipherEncryption.py
+++ cipherEncryption.py
@@ -1,4 +1,6 @@
 def encrypt(s,k):
+    if k==0:
+        return "INVALID INPUT"
     s1=""
     for char in s:
         if 'A'<=char<='Z':
@@ -13,8 +15,16 @@ def encrypt(s,k):
             idx=ord(char)-ord('0')
             idx=(idx+k)%10
             s1+=chr(ord('0')+idx)
+        else:
+            s1+=char
     return s1
 
 s="AdyZ89"
+s1="All the best"
+k1=1
+print(encrypt(s1,k1))
 k=2
 print(encrypt(s,k))
+s3="sample"
+k3=0
+print(encrypt(s3,k3))
