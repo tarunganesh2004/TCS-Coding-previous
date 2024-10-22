2e81d738467dc3096cd7b5e6154d2b901f30aca5
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-10-22 21:31:41 +0530

    basics

---
+++ basics/test.py
@@ -0,0 +1,24 @@
+# # from collections import Counter
+
+# # l=[1, 2,3,4, 5,6,1 ,2 ,3 ,2 ,3 ,4,1,1,1,1,4]
+# # c=Counter(l)
+
+# # for i in c:
+# #     print(f"Value {i} has {c[i]} occurence(s)")
+
+# # convert number to binary
+# # n=10
+# # s=bin(n)[2:]
+# # print(s)
+
+# # # convert number to hexadecimal
+# # n=17
+# # s=hex(n)[2:]
+# # print(s)
+
+# import decimal
+
+# # convert binary to decimal
+# s='10101'
+# n=int(s,2)
+# print(n)
