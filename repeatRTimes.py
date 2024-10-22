2ba73d9204f10bdb266ae63d1863004b1e236d53
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-10-22 22:05:36 +0530

    repeatledly sum

--- repeatRTimes.py
+++ repeatRTimes.py
@@ -13,3 +13,24 @@ def single_digit_sum(n,r):
 n=int(input())
 r=int(input())
 print(single_digit_sum(n,r))
+
+def another_way(n,r):
+    digit_sum=0
+
+    while n>0:
+        d=n%10
+        digit_sum+=d
+        n//=10
+
+    t=digit_sum*r
+
+    res=0
+
+    while t>0:
+        d=t%10
+        res+=d
+        t//=10
+
+    return res
+
+print(another_way(n,r))
