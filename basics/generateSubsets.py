0fefe6af1da71ebe0b4249b3ed9c9b89ba4cbad9
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-10-23 21:13:46 +0530

    subsets

--- basics/generateSubsets.py
+++ basics/generateSubsets.py
@@ -6,5 +6,13 @@ def generate(nums,index=0,cur=[]):
     generate(nums,index+1,cur)
     generate(nums,index+1,cur+[nums[index]])
 
+def another_way(nums):
+    subsets=[[]]
+    for num in nums:
+        new_subsets=[cur+[num] for cur in subsets]
+        subsets.extend(new_subsets)
+
+    return subsets
 nums=[1,2,3]
 generate(nums)
+print(another_way(nums))
