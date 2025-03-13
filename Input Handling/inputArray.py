dc6e2560c400fbae1f0659fe3ce9b8a5d66e5e72
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-13 17:52:36 +0530

    Enhance input handling for various array formats

--- Input Handling/inputArray.py
+++ Input Handling/inputArray.py
@@ -9,3 +9,12 @@ print(arr)
 # input 1,2,3,4,5
 arr=list(map(int,input().split(",")))
 print(arr)
+
+
+# ['A','B','C','D','E']
+arr=input().strip("[]").replace("'","").split(",")
+print(arr)
+
+# input A B C D E
+arr=input().split() # split input by spaces
+print(arr)
