3e16112d4fbef1e19f0ffb0dd94b7a5e2b2ac5bc
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-18 21:21:48 +0530

    Refactor inputArray.py to include examples for single and multiple character input handling

--- Input Handling/inputArray.py
+++ Input Handling/inputArray.py
@@ -1,38 +1,49 @@
-# input [1,2,3,4,5]
-arr=list(map(int,input().strip("[]").split(",")))
-print(arr)
+# # input [1,2,3,4,5]
+# arr=list(map(int,input().strip("[]").split(",")))
+# print(arr)
 
-# input 1 2 3 4 5
-arr=list(map(int,input().split()))
-print(arr)
+# # input 1 2 3 4 5
+# arr=list(map(int,input().split()))
+# print(arr)
 
-# input 1,2,3,4,5
-arr=list(map(int,input().split(",")))
-print(arr)
+# # input 1,2,3,4,5
+# arr=list(map(int,input().split(",")))
+# print(arr)
 
 
-# ['A','B','C','D','E']
-arr=input().strip("[]").replace("'","").split(",")
-print(arr)
+# # ['A','B','C','D','E']
+# arr=input().strip("[]").replace("'","").split(",")
+# print(arr)
 
-# input A B C D E
-arr=input().split() # split input by spaces
-print(arr)
+# # input A B C D E
+# arr=input().split() # split input by spaces
+# print(arr)
 
-# matrix
-# input [[1,2,3],[4,5,6],[7,8,9]]
-matrix=[list(map(int,row.strip("[]").split(","))) for row in input().strip("[]").split("],")]
-print(matrix)
+# # matrix
+# # input [[1,2,3],[4,5,6],[7,8,9]]
+# matrix=[list(map(int,row.strip("[]").split(","))) for row in input().strip("[]").split("],")]
+# print(matrix)
 
-# input 1 2 3 4 5 6 7 8 9
-matrix=[list(map(int,input().split()) for i in range(3))]
-print(matrix)
+# # input 1 2 3 4 5 6 7 8 9
+# matrix=[list(map(int,input().split()) for i in range(3))]
+# print(matrix)
 
-n=int(input())
-m=[]
-for _ in range(n):
-    arr=list(map(int,input().split()))
-    m.append(arr)
-# print(m)
-for row in m:
-    print(" ".join(map(str,row)))
+# n=int(input())
+# m=[]
+# for _ in range(n):
+#     arr=list(map(int,input().split()))
+#     m.append(arr)
+# # print(m)
+# for row in m:
+#     print(" ".join(map(str,row)))
+
+
+# single character input
+# input A
+c=input()[0]
+print(c)
+
+# take multiple characters as input
+
+chars=list(input().strip())
+print(chars)
