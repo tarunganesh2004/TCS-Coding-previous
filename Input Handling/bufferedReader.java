95f67cab9088da6952f2036058bc69ac63c0326a
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-18 20:49:39 +0530

    Refactor main method in bufferedReader to read a single character and a line of input

--- Input Handling/bufferedReader.java
+++ Input Handling/bufferedReader.java
@@ -20,26 +20,42 @@ class test {
     //     }
     // }
 
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] a = null;
-        try{
-             int n = Integer.parseInt(br.readLine());
+    // public static void main(String[] args) {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     int[] a = null;
+    //     try{
+    //          int n = Integer.parseInt(br.readLine());
 
-        // read space separated elements
-        String[] arr = br.readLine().split(" ");
-        a = new int[n];
-        for(int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(arr[i]);
-        }
+    //     // read space separated elements
+    //     String[] arr = br.readLine().split(" ");
+    //     a = new int[n];
+    //     for(int i = 0; i < n; i++) {
+    //         a[i] = Integer.parseInt(arr[i]);
+    //     }
+    // } catch (IOException e) {
+    //     e.printStackTrace();
+    // }
+    // if (a != null) {
+    //         for (int i : a) {
+    //         System.out.print(i + " ");
+    //     }
+    //     }
+
+    // }
+public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // String
+    String str = "";
+    char c=' ';
+    try{
+        str = br.readLine();
+        // reading a single character
+        c = (char) br.read();
+
     } catch (IOException e) {
         e.printStackTrace();
+
     }
-    if (a != null) {
-            for (int i : a) {
-            System.out.print(i + " ");
-        }
-        }
-       
-    }
+    System.out.println(str+" "+c);
+}
 }
