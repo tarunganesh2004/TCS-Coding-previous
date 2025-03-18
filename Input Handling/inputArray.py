3f05c975699966d3a7e8274cd2a7df2bb007b246
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-18 21:14:40 +0530

    Enhance inputArray.py to support reading matrices and multiple integer arrays from input

--- Input Handling/inputArray.py
+++ Input Handling/inputArray.py
@@ -18,3 +18,21 @@ print(arr)
 # input A B C D E
 arr=input().split() # split input by spaces
 print(arr)
+
+# matrix
+# input [[1,2,3],[4,5,6],[7,8,9]]
+matrix=[list(map(int,row.strip("[]").split(","))) for row in input().strip("[]").split("],")]
+print(matrix)
+
+# input 1 2 3 4 5 6 7 8 9
+matrix=[list(map(int,input().split()) for i in range(3))]
+print(matrix)
+
+n=int(input())
+m=[]
+for _ in range(n):
+    arr=list(map(int,input().split()))
+    m.append(arr)
+# print(m)
+for row in m:
+    print(" ".join(map(str,row)))
