650608649f60b6e9a04bc83826d9065d6dd4d347
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-18 21:33:25 +0530

    Refactor usingsys.py to enhance input handling for multiple test cases and streamline matrix input processing

--- Input Handling/usingsys.py
+++ Input Handling/usingsys.py
@@ -1,26 +1,36 @@
 import sys
 
-# # read entire input at once and split into a list
-# data=sys.stdin.read().split()
-# # print(data)
+# # # read entire input at once and split into a list
+# # data=sys.stdin.read().split()
+# # # print(data)
 
-# n=int(data[0])
+# # n=int(data[0])
 
-# arr=list(map(int,data[1:n+1]))
+# # arr=list(map(int,data[1:n+1]))
+# # print(arr)
+
+# # line by line input
+# s=sys.stdin.readline().strip()
+# n=int(sys.stdin.readline())
+# arr=list(map(int,sys.stdin.readline().split()))
+
+# print(s)
+# print(n)
 # print(arr)
 
-# line by line input
-s=sys.stdin.readline().strip()
-n=int(sys.stdin.readline())
-arr=list(map(int,sys.stdin.readline().split()))
+# # reading a 2d matrix
+# n,m=map(int,sys.stdin.readline().split())
 
-print(s)
-print(n)
-print(arr)
+# matrix=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+# for row in matrix:
+#     print(" ".join(map(str,row)))
 
-# reading a 2d matrix
-n,m=map(int,sys.stdin.readline().split())
+# # for character
+# ch=sys.stdin.read(1)
 
-matrix=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
-for row in matrix:
-    print(" ".join(map(str,row)))
+# handling multiple test cases
+t=int(sys.stdin.readline())
+for _ in range(t):
+    # n=int(sys.stdin.readline())
+    arr=list(map(int,sys.stdin.readline().split()))
+    print(" ".join(map(str,arr)))
