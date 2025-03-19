e3b59b08ea90b4ff614427426147a4fa905f7b30
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-19 12:03:03 +0530

    Enhance Java example to include map usage for counting occurrences of array elements

--- Array/javaSyntax.java
+++ Array/javaSyntax.java
@@ -2,12 +2,27 @@ import java.util.*;
 
 class test {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr = { 1, 2, 3, 4, 5,1 };
         // arr is primitive type
         // to convert arr as list we should convert it to Integer[]
-        Integer[] arr1 = { 1, 2, 3, 4, 5 };
-        List<Integer> list = Arrays.asList(arr1);
-        System.out.println(list);
-        list.add(6); // this will give error.because list is fixed size
+        // Integer[] arr1 = { 1, 2, 3, 4, 5 };
+        // List<Integer> list = Arrays.asList(arr1);
+        // System.out.println(list);
+        // list.add(6); // this will give error.because list is fixed size
+
+        // Map usage
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int num : arr) {
+            if (m.containsKey(num)) {
+                m.put(num, m.get(num) + 1);
+            } else {
+                m.put(num, 1);
+            }
+        }
+        System.out.println(m);
+        // iterating
+        for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
     }
 }
