052946a041b5e07b1e6045fe61c61f653c52669b
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-19 14:06:37 +0530

    Add Java example demonstrating queue, stack, and priority queue usage

--- Array/javaSyntax.java
+++ Array/javaSyntax.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class test {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5,1 };
+        int[] arr = { 1, 2, 3, 4, 5, 1 };
         // arr is primitive type
         // to convert arr as list we should convert it to Integer[]
         // Integer[] arr1 = { 1, 2, 3, 4, 5 };
@@ -11,18 +11,74 @@ class test {
         // list.add(6); // this will give error.because list is fixed size
 
         // Map usage
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int num : arr) {
-            if (m.containsKey(num)) {
-                m.put(num, m.get(num) + 1);
-            } else {
-                m.put(num, 1);
-            }
-        }
-        System.out.println(m);
+        // Map<Integer, Integer> m = new HashMap<>();
+        // for (int num : arr) {
+        // if (m.containsKey(num)) {
+        // m.put(num, m.get(num) + 1);
+        // } else {
+        // m.put(num, 1);
+        // }
+        // }
+        // System.out.println(m);
         // iterating
-        for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
-            System.out.println(entry.getKey()+" "+entry.getValue());
+        // for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+        // System.out.println(entry.getKey()+" "+entry.getValue());
+        // }
+        // Arraylist usage
+        // ArrayList<Integer> l = new ArrayList<>();
+        // for (int num : arr) {
+        //     l.add(num);
+        // }
+        // System.out.println(l);
+        // l.add(6);
+        // l.add(2, 7); // .add(index,element) is used to add element at index
+        // System.out.println(l);
+        // l.remove(l.size() - 1);
+        // l.set(0, 0); // .set(index,element) is used to set element at index
+        // System.out.println(l.contains(1)); // .contains(element) is used to check if element is present in list
+        // for (int i = 0; i < l.size(); i++) {
+        //     System.out.println(l.get(i)); // .get is used to get the element at index i
+        // }
+        // System.out.println(l.indexOf(l.size() - 1)); // .indexOf(element) is used to get the index of element
+        // set usage
+        // Set<Integer> s = new HashSet<>();
+        // for (int num : arr) {
+        //     if (s.contains(num)) {
+        //         System.out.println("Duplicate element is " + num);
+        //     } else {
+        //         s.add(num);
+        //     }
+        // }
+        // System.out.println(s);
+        // s.remove(3);
+        // System.out.println(s);
+
+        // Queue
+        Queue<Integer> q = new LinkedList<>();
+        for (int num : arr) {
+            q.offer(num); // .offer is used to add element in queue or q.add(num)
+
         }
+        System.out.println(q.poll());
+        System.out.println(q.peek());
+        System.out.println(q.isEmpty());
+
+        // Stack
+        Stack<Integer> st = new Stack<>();
+        for (int num : arr) {
+            st.push(num); // .push is used to add element in stack
+        }
+        System.out.println(st.pop());
+        System.out.println(st.peek());
+        System.out.println(st.isEmpty());
+
+        // Priority queue
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // max heap
+        for (int num : arr) {
+            maxHeap.offer(num);
+        }
+        System.out.println(maxHeap.poll());
+        System.out.println(maxHeap.peek());
+        System.out.println(maxHeap.isEmpty());
     }
 }
